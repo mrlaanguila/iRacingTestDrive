20b55cf54041d57ba7dc4cd693e3779c409150b5
Ander <Ander@DESKTOP-DOJV5A2>
2019-03-14 18:35:06 +0100

    UI mejorada

--- iRacing/src/iRacing/Launcher.java
+++ iRacing/src/iRacing/Launcher.java
@@ -5,11 +5,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
 
 public class Launcher {
 
@@ -61,12 +64,12 @@ public class Launcher {
 		for (int i = 1; i < cars.size; i++) {
 			CarList.addItem(cars.array[i].nombre);
 		}
-		CarList.setBounds(101, 56, 235, 20);
+		CarList.setBounds(101, 70, 235, 20);
 
 		frmIracingTestDrive.getContentPane().add(CarList);
 
 		JComboBox<String> TrackList = new JComboBox<String>();
-		TrackList.setBounds(101, 87, 235, 20);
+		TrackList.setBounds(101, 101, 235, 20);
 		for (int i = 1; i < tracks.size; i++) {
 			TrackList.addItem(tracks.array[i].nombre);
 		}
@@ -75,50 +78,56 @@ public class Launcher {
 		JComboBox<String> WeatherBox = new JComboBox<String>();
 		WeatherBox.setModel(
 				new DefaultComboBoxModel(new String[] { "Clear", "Partly Cloudy", "Mostly Cloudy", "Overcast" }));
-		WeatherBox.setBounds(227, 118, 109, 20);
+		WeatherBox.setBounds(227, 132, 109, 20);
 		frmIracingTestDrive.getContentPane().add(WeatherBox);
 
 		txtTemperatura = new JTextField();
 		txtTemperatura.setToolTipText("Temperatura");
-		txtTemperatura.setBounds(101, 118, 109, 20);
+		txtTemperatura.setBounds(101, 132, 109, 20);
 		frmIracingTestDrive.getContentPane().add(txtTemperatura);
 		txtTemperatura.setColumns(10);
 
 		textMes = new JTextField();
-		textMes.setBounds(101, 149, 25, 20);
+		textMes.setBounds(101, 163, 25, 20);
 		frmIracingTestDrive.getContentPane().add(textMes);
 		textMes.setColumns(10);
 
 		textDia = new JTextField();
-		textDia.setBounds(145, 149, 25, 20);
+		textDia.setBounds(145, 163, 25, 20);
 		frmIracingTestDrive.getContentPane().add(textDia);
 		textDia.setColumns(10);
 
 		JTextArea textArea = new JTextArea();
-		textArea.setEnabled(false);
+		textArea.setBackground(UIManager.getColor("Button.background"));
 		textArea.setEditable(false);
 		textArea.setText("-");
-		textArea.setBounds(130, 147, 12, 22);
+		textArea.setBounds(130, 161, 12, 22);
 		frmIracingTestDrive.getContentPane().add(textArea);
 
 		JTextArea txtrMes = new JTextArea();
+		txtrMes.setEditable(false);
+		txtrMes.setBackground(UIManager.getColor("Button.background"));
 		txtrMes.setText("Mes");
-		txtrMes.setBounds(101, 172, 25, 22);
+		txtrMes.setBounds(101, 186, 25, 22);
 		frmIracingTestDrive.getContentPane().add(txtrMes);
 
 		JTextArea txtrDia = new JTextArea();
+		txtrDia.setEditable(false);
+		txtrDia.setBackground(UIManager.getColor("Button.background"));
 		txtrDia.setText("Dia");
-		txtrDia.setBounds(145, 172, 25, 22);
+		txtrDia.setBounds(145, 186, 25, 22);
 		frmIracingTestDrive.getContentPane().add(txtrDia);
 
 		textHora = new JTextField();
-		textHora.setBounds(258, 149, 46, 20);
+		textHora.setBounds(258, 163, 46, 20);
 		frmIracingTestDrive.getContentPane().add(textHora);
 		textHora.setColumns(10);
 
 		JTextArea txtrHhmm = new JTextArea();
+		txtrHhmm.setEditable(false);
+		txtrHhmm.setBackground(UIManager.getColor("Button.background"));
 		txtrHhmm.setText("HH:MM");
-		txtrHhmm.setBounds(258, 172, 46, 22);
+		txtrHhmm.setBounds(258, 186, 46, 22);
 		frmIracingTestDrive.getContentPane().add(txtrHhmm);
 
 		JButton btnLaunch = new JButton("Launch");
@@ -141,6 +150,10 @@ public class Launcher {
 		btnLaunch.setBounds(167, 227, 89, 23);
 		frmIracingTestDrive.getContentPane().add(btnLaunch);
 
-	}
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Ander\\Downloads\\iRacing.png"));
+		lblNewLabel.setBounds(0, 0, 434, 59);
+		frmIracingTestDrive.getContentPane().add(lblNewLabel);
 
+	}
 }
